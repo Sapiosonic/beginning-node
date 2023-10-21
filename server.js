02c5d0196d3d1cc55ce45139b2f4ca90654f4397
Radamés Rodrigues <radames.idiomas@gmail.com>
2023-10-21 15:27:15 -0300

    Starting with Express

--- server.js
+++ server.js
@@ -1,27 +1,7 @@
-// require takes the name as a string argument and returns the module
-const http = require('http');
-// File System module helps us to interact with the files in our server
-const fs = require('fs');
+const express = require('express');
 
-const homePage = fs.readFileSync('index.html');
-const aboutPage = fs.readFileSync('about.html');
-const contactPage = fs.readFileSync('contact.html');
-const notFoundPage = fs.readFileSync('not-found.html');
+const app = express();
 
-
-// http.createServer takes a callback function
-const server = http.createServer((req, resp) =>{
-  if(req.url === '/about')
-    resp.end(aboutPage);
-  else if(req.url === '/contact')
-    resp.end(contactPage);
-  else if(req.url === '/')
-    resp.end(homePage);
-  else{
-    resp.writeHead(404);
-    resp.end(notFoundPage);
-  }
+app.listen(3000, () => {
+  console.log('App listening on port 3000');
 });
-
-// listen to a port
-server.listen(3000);
