15bb284f2fda63e91b1e853bf2a2f837dda1d71c
Radamés Rodrigues <radames.idiomas@gmail.com>
2023-10-21 16:49:59 -0300

    Routing multiple urls with Express

--- server.js
+++ server.js
@@ -10,3 +10,11 @@ app.listen(3000, () => {
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.resolve(__dirname,'about.html'));
+});
+
+app.get('/contact', (req, res) =>{
+  res.sendFile(path.resolve(__dirname, 'contact.html'));
+});
