791dd2e989dc9e42e16374e0e5b41526a8431e58
Radamés Rodrigues <radames.idiomas@gmail.com>
2023-10-21 13:37:41 -0300

    Useful explanation

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+// require takes the name as a string argument and returns the module
 const http = require('http');
 
+// http.createServer takes a callback function
 const server = http.createServer((req, resp) =>{
+  // send a request
   console.log(req.url);
+  // send the response
   resp.end('Hello World');
 });
 
+// listen to a port
 server.listen(3000);
