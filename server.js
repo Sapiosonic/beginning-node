1691f24cd2d7bc4c34599f3cfabac227c7831269
Radamés Rodrigues <radames.idiomas@gmail.com>
2023-10-21 14:51:41 -0300

    answering requests with HTML files

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 // require takes the name as a string argument and returns the module
 const http = require('http');
+// File System module helps us to interact with the files in our server
+const fs = require('fs');
+
+const homePage = fs.readFileSync('index.html');
+const aboutPage = fs.readFileSync('about.html');
+const contactPage = fs.readFileSync('contact.html');
+const notFoundPage = fs.readFileSync('not-found.html');
+
 
 // http.createServer takes a callback function
 const server = http.createServer((req, resp) =>{
   if(req.url === '/about')
-    resp.end('The about page');
+    resp.end(aboutPage);
   else if(req.url === '/contact')
-    resp.end('The contact page');
+    resp.end(contactPage);
   else if(req.url === '/')
-    resp.end('The home page');
+    resp.end(homePage);
   else{
     resp.writeHead(404);
-    resp.end('Page not found');
+    resp.end(notFoundPage);
   }
 });
 
