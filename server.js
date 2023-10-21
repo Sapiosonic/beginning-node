0eb77d51e3b71627d3907a96f7136fa8779d8ee6
Radamés Rodrigues <radames.idiomas@gmail.com>
2023-10-21 16:39:39 -0300

    Request handling example with Express

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const path = require('path')
 
 const app = express();
 
 app.listen(3000, () => {
   console.log('App listening on port 3000');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
